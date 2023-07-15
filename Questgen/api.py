2cc44784bcee333cd170cdc0a9cc0996a6575be2
hoangviet2 <hoangviet18306@gmail.com>
2023-07-15 12:01:05 +0700

    let's go

--- Questgen/api.py
+++ Questgen/api.py
@@ -9,29 +9,33 @@ from pprint import pprint
 from Questgen import main
 import os
 import json
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 qg = main.QGen()
-
 @app.route("/")
 def hello():
     return "Welcome to machine learning model APIs!"
 
 @app.route("/predict", methods=["POST"])
 def predict():
+
     if qg:
         try:
+            print(type(request))
+            print(request.json)
             jsondata = request.json
+            if len(jsondata["input_text"]) == 0:
+                return jsonify({'error': "Find 0 character in the input"})
             print(type(jsondata))
             prediction = qg.predict_mcq(jsondata)
             package = json.dumps(prediction)
             return package
-
         except:
             return jsonify({'trace': traceback.format_exc()})
     else:
         print('Train the model first')
-        return('No model here to use')
+        return jsonify({'error': "find a model"})
 
 
 if __name__ == '__main__':
