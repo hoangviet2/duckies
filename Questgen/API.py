bfc0c597e1c83880e6c73c5ff30727b7728eab82
hoangviet2 <hoangviet18306@gmail.com>
2023-07-16 13:17:12 +0700

    set

--- Questgen/API.py
+++ Questgen/API.py
@@ -39,7 +39,8 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = 12345
+    #port = 12345
     print('Model columns loaded')
-    app.run(port=port,debug=True)
+    #app.run(port=port,debug=True)
+    app.run(debug=True)
 
