52611e01fcb8774a85126cfa9a683d2472c4ecb7
hoangviet2 <hoangviet18306@gmail.com>
2023-07-21 11:57:55 +0700

    abcdefg

--- Questgen/clone.py
+++ Questgen/clone.py
@@ -1,7 +1,5 @@
 import wget
 import tarfile
-import spacy
-nlp = spacy.load("en_core_web_sm")
 def run():
     url = "https://github.com/explosion/sense2vec/releases/download/v1.0.0/s2v_reddit_2015_md.tar.gz"
     filename = wget.download(url)
